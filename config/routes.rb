221d2e4e81fa62e2477ee2e4bfaeebda8153016f
voblav <voblav.aukro@gmail.com>
2018-07-23 22:30:15 +0300

    add products_controll and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  root to: 'categories#index'
+
+  resources :categories, only: [:index] do
+    resources :products, only: [:index]
+  
+  end
+
 end
