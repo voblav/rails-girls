e360e6554e917579f0ea0735575f34e855a05308
voblav <voblav.aukro@gmail.com>
2018-07-23 20:42:33 +0300

    add categor_controll and view

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,7 @@
 class CategoriesController < ApplicationController
 
   def index
-    @categories = Category.order(:title)
+    @categories = Category.joins(:products).select('categories.*, count(products.id) as products_count').group('categories.id').order(:title)
   end
 
 end
